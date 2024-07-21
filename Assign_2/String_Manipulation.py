d5d64bcbf9fdd79ce3612403be755061dca6efc1
Harshit Patel <harshit.patel@calsoftinc.com>
2024-07-21 20:10:49 +0530

    Added String_Maniupulation.py File

---
+++ Assign_2/String_Manipulation.py
@@ -0,0 +1,18 @@
+str = "hello, World! Welcome to the Python string manipulation demo."
+
+# Converts first letter to uppercase
+print(str.capitalize()) 
+# Output: Hello, world! welcome to the python string manipulation demo.
+
+# Converts to lowercase
+print(str.lower()) 
+# Output: hello, world! welcome to the python string manipulation demo.
+
+# Counts the word
+x = str.count("l")
+print(x) 
+# Output: 5
+
+y = str.find("Python")
+print(y) 
+# Output: 29
